32b6f68afaefec7bf4adcbcca74129447f5723a0
YUSRA SALEEM <rayyan726.m@gmail.com>
2024-05-18 03:54:00 +0500

    this is my simple-CLI-Calculator

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 #! /usr/bin/env node
-import inquirer from "inquirer";
-let answer = await inquirer.prompt([{
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
+let answer = await inquirer_1.default.prompt([{
         message: "Enter your First Number",
         type: "number",
         name: "firstNumber"
